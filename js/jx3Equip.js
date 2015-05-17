2a3cf02abcdbb070fc37e011de78c1526ba2be2b
shine <shine130@vip.qq.com>
2015-05-17 15:42:23 +0800

    add code

---
+++ js/jx3Equip.js
@@ -0,0 +1,34 @@
+$(function(){
+	var ctnBox=$("#box"),
+		pName=ctnBox.find(".pName"),
+		pInfo=ctnBox.find(".pInfo"),
+		pBody=ctnBox.find(".pBody"),
+		spirit=ctnBox.find(".spirit"),
+		attack=ctnBox.find(".attack"),
+		overcome=ctnBox.find(".overcome"),
+		huajing=ctnBox.find(".huajing"),
+		hole=ctnBox.find(".hole"),
+		quality=ctnBox.find(".quality"),
+		score=ctnBox.find(".score"),
+		menpai=ctnBox.find(".menpai");
+
+	$("#armList .opt").bind('mouseover',function(){
+		var index=parseInt($(this).attr("data-arm"))-1;
+		$.getJSON('data/arminfo.json',function(data){
+			var holeval=(data[index].hole).join('');
+			pName.html(data[index].pName);
+			pInfo.html(data[index].pInfo);
+			pBody.html(data[index].pBody);
+			spirit.html(data[index].spirit);
+			attack.html(data[index].attack);
+			overcome.html(data[index].overcome);
+			huajing.html(data[index].huajing);
+			hole.html(holeval);
+			quality.html(data[index].quality);
+			score.html(data[index].score);
+			menpai.html(data[index].menpai);
+
+
+		})
+	});
+});
